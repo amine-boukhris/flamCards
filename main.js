ecf8af09e9f48773803f1cbf6df442ecb765108e
amine-boukhris <110597555+amine-boukhris@users.noreply.github.com>
2022-10-09 15:26:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ const card = document.getElementById("card");
 const nextBtn = document.getElementById("next");
 
 let words = {
-    "word 1": "ans 1",
-    "word 2": "ans 2",
-    "word 4": "ans 4",
-    "word 5": "ans 5",
-    "word 6": "ans 6",
-    "word 7": "ans 7",
     "die Katze": "the cat",
     "der Vogel": "the bird",
     "der Hund": "the dog"
@@ -45,7 +39,6 @@ nextBtn.addEventListener('click', () => {
     wordsLength = Object.keys(words).length;
     if (wordsLength !== 0) {document.getElementById('main-word').innerHTML = Object.keys(words)[Math.floor(Math.random() * wordsLength)];} else {document.getElementById("main-word").innerHTML = "you are done!"}
     
-    console.log(words);
 })
 
 
